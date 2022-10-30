563dcd0b756ef7169aec0a9631e8783d553daf47
Jbarajas07 <113144498+Jbarajas07@users.noreply.github.com>
2022-10-29 21:34:11 -0500

    Add files via upload

---
+++ Section 1 - Day 1 - Variables/Exercises/Exercise 3 - Input Function.py
@@ -0,0 +1,11 @@
+#Write a program that prints the number of characters in a user's name.
+
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+
+
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
---
+++ Section 1 - Day 1 - Variables/Notes/string manipulation.py
@@ -0,0 +1,14 @@
+# Break lines with \n
+print("Hello world!\nHello world!\nHello world!")
+
+# concatination
+print("Hello " + " Jesus")
+print("Hello" + "" + "Jesus")
+
+
+# input() will get user input in console
+# Then print() will print the world "Hello" and the user input
+input("What is your name?")
+print("Hello " + input("What is your name?") + "!")
+
+
